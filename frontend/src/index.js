c08dc5a4085d05a01c2054716886e779977e153c
cheerypal <ejg9@hw.ac.uk>
2021-07-03 10:35:16 +0100

    added navigation, improved code modularity

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,18 +5,23 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 //redux
-// redux imports //
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 
+// navigator
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
